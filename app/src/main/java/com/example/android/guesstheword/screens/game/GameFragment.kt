cafaa95b72e31f6c58ee282772dd23b51d0dc682
Nichanun <nichanun.pl@rmuti.ac.th>
2020-08-03 23:26:04 +0700

    Implement click listener for the End Game button

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -78,6 +78,7 @@ class GameFragment : Fragment() {
     }
 
     private fun onEndGame() {
+        gameFinished()
     }
 
 
@@ -89,5 +90,15 @@ class GameFragment : Fragment() {
 
     private fun updateScoreText() {
         binding.scoreText.text = viewModel.score.toString()
+    }
+
+    /**
+     * Called when the game is finished
+     */
+    private fun gameFinished() {
+        Toast.makeText(activity, "Game has just finished", Toast.LENGTH_SHORT).show()
+        val action = GameFragmentDirections.actionGameToScore()
+        action.score = viewModel.score
+        NavHostFragment.findNavController(this).navigate(action)
     }
 }
